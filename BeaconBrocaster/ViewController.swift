d44685cffdfd3f8de24ae5e78de6230dc030a001
Xinyi Zhuang <yingmu52@msn.com>
2018-03-24 16:55:06 -0700

    Initial Commit

---
+++ BeaconBrocaster/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BeaconBrocaster
+//
+//  Created by Xinyi Zhuang on 24/03/2018.
+//  Copyright © 2018 x52. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
