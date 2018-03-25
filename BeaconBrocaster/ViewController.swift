96121b3c51ba96c54a08fa7f24290c8516b9a30e
Xinyi Zhuang <yingmu52@msn.com>
2018-03-24 19:51:17 -0700

    add beacon detector

--- BeaconBrocaster/ViewController.swift
+++ BeaconBrocaster/ViewController.swift
@@ -7,19 +7,60 @@
 //
 
 import UIKit
+import CoreLocation
+import CoreBluetooth
+import Snakepit
 
 class ViewController: UIViewController {
+  @IBOutlet var textView: UITextView!
+  @IBOutlet var button: UIButton!
+
+  lazy var manager: CLLocationManager = {
+    let mgr = CLLocationManager()
+    mgr.delegate = self
+    return mgr
+  }()
+
+  let beaconRegion: CLBeaconRegion = {
+    let uuid = UUID(uuidString: "05F62A3D-F60F-44BC-B36E-2B80FD6C9679")!
+    let identifier = UIDevice.current.identifierForVendor?.uuidString
+    return CLBeaconRegion(proximityUUID: uuid, major: 52, minor: 88, identifier: identifier!)
+  }()
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    manager.requestWhenInUseAuthorization()
+    textView.text = nil
+    button.setTitle("Start", for: .normal)
+    button.setTitle("Monitoring...", for: .selected)
   }
 
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
+  @IBAction func buttonPressed(_ sender: UIButton) {
+    sender.isSelected = !sender.isSelected
+    if sender.isSelected {
+      manager.startRangingBeacons(in: beaconRegion)
+//      manager.stopMonitoring(for: beaconRegion)
+    } else {
+      manager.stopRangingBeacons(in: beaconRegion)
+//      manager.startMonitoring(for: beaconRegion)
+    }
   }
-
-
 }
 
+extension ViewController: CLLocationManagerDelegate {
+  func locationManager(_ manager: CLLocationManager, didEnterRegion region: CLRegion) {
+//    manager.startRangingBeacons(in: beaconRegion)
+    textView.text = "Entered Region"
+  }
+
+  func locationManager(_ manager: CLLocationManager, didExitRegion region: CLRegion) {
+//    manager.stopRangingBeacons(in: beaconRegion)
+    textView.text = "Existed Region"
+  }
+  func locationManager(_ manager: CLLocationManager, didRangeBeacons beacons: [CLBeacon], in region: CLBeaconRegion) {
+    textView.text = "Ranged Beacon Region\n"
+    for b in beacons {
+      textView.text = textView.text + String(b.description)
+    }
+  }
+}
